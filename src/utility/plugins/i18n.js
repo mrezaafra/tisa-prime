ab15e4417fb20c571a32bfe55a6e7ade13afe03c
Afra <m.afra@norahand.com>
2025-12-25 14:42:40 +0330

    lang upgrade

--- src/utility/plugins/i18n.js
+++ src/utility/plugins/i18n.js
@@ -1,4 +1,4 @@
-import {createI18n} from "vue-i18n";
+import { createI18n } from "vue-i18n";
 
 const globalLocale = import.meta.glob('@/locales/**.fa.json', {eager: true})
 const componentLocales = import.meta.glob('@/components/**/locales/**.fa.json', {eager: true})
@@ -7,19 +7,19 @@ const locales = import.meta.glob('@/pages/**/locales/**.fa.json', {eager: true})
 const messages = {}
 
 let sources = {
-  ...globalLocale,
-  ...componentLocales,
-  ...locales
+    ...globalLocale,
+    ...componentLocales,
+    ...locales
 }
 
 for (const path in sources) {
-  Object.assign(messages, sources[path])
+    Object.assign(messages, sources[path])
 }
 
 export const i18n = createI18n({
     locale: 'fa',
-  fallbackLocale: 'fa',
-  legacy: false,
-  globalInjection: true,
-  messages: {fa: messages}
+    fallbackLocale: 'fa',
+    legacy: false,
+    globalInjection: true,
+    messages: {fa: messages}
 })
